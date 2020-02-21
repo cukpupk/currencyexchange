abf2b8c32c1c0e577816b99b8fe7d2fbcb7994a3
cukpupk <cukpuk@gmail.com>
2020-02-21 14:51:38 -0800

    Updates

---
+++ precache-manifest.3d5d384c7eeff765f9fd3ba65a068653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cef222dff55ab933aa96ddcb5e20675",
+    "url": "/currencyexchange/index.html"
+  },
+  {
+    "revision": "dfe1ded0948dceaa2fbb",
+    "url": "/currencyexchange/static/css/main.e8048faf.chunk.css"
+  },
+  {
+    "revision": "177c2afff737e9a98a51",
+    "url": "/currencyexchange/static/js/2.94635ea9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/currencyexchange/static/js/2.94635ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe1ded0948dceaa2fbb",
+    "url": "/currencyexchange/static/js/main.eaf3ef30.chunk.js"
+  },
+  {
+    "revision": "fcbf1c4290dd1a543a8f",
+    "url": "/currencyexchange/static/js/runtime-main.49992ec6.js"
+  },
+  {
+    "revision": "8ebfab8f167e6723aff8a3646414f245",
+    "url": "/currencyexchange/static/media/money.8ebfab8f.png"
+  }
+]);
